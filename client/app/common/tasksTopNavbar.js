e5faf55970397e206266bbd93009c320f4bf3854
Sergey Smirnov <s@tigr.bz>
2016-01-11 00:02:33 +0300

    navbar lang toggle.

--- client/app/common/tasksTopNavbar.js
+++ client/app/common/tasksTopNavbar.js
@@ -5,6 +5,42 @@ Template.tasksTopNavbar.rendered = function(){
     // $('body').addClass('fixed-nav');
     // $(".navbar-static-top").removeClass('navbar-static-top').addClass('navbar-fixed-top');
 
+    // i18n
+
+    $.i18n.init({
+        resGetPath: 'locales/__lng__.json',
+        load: 'unspecific',
+        fallbackLng: false,
+        lng: 'ru'
+    }, function (t){
+        $('.i18container').i18n();
+        $('#side-menu').i18n();
+        $('.navbar-top-links').i18n();
+    });
+
+
+    $('.set_en a').on('click', function (){
+        i18n.setLng('en', function(){
+            $('.i18container').i18n();
+            $('#side-menu').i18n();
+            $('.navbar-top-links').i18n();
+
+            $('.set_en').addClass('active');
+            $('.set_ru').removeClass('active');
+        });
+    });
+
+    $('.set_ru a').on('click', function (){
+        i18n.setLng('ru', function(){
+            $('.i18container').i18n();
+            $('#side-menu').i18n();
+            $('.navbar-top-links').i18n();
+
+            $('.set_ru').addClass('active');
+            $('.set_en').removeClass('active');
+        });
+    });
+
 };
 
 Template.tasksTopNavbar.events({
